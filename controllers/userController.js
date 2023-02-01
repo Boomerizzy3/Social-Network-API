a9d832f067a3462fde38a2e13daa3fee8121cb0b
Tim Mardis <mardistj96@yahoo.com>
2023-01-31 19:02:57 -0600

    Added get and psot routes

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const { ObjectId } = require('mongoose').Types;
+const { User } = require('../models');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users
+        //   headCount: await headCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  }
+};
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+    }
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
